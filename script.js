bfdb2997f421e4efdda70372c1a86bb247adef61
harshubari <121845828+harshubari@users.noreply.github.com>
2024-01-11 01:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function submitForm() {
+
+    var firstName = document.getElementById('name').value;
+    var lastName = document.getElementById('lname').value;
+    var phoneNumber = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+	
+    var result = "First Name: " + name + "\n"
+               + "Last Name: " + lname + "\n"
+               + "Phone Number: " + phone + "\n"
+               + "Email ID: " + email;
+    alert(result);
+}
